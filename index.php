9925a9dc33a6a33d7c1d214dfb723ff05ef01120
danvop <danvop@gmail.com>
2015-04-19 11:31:00 +1000

    adding table construction function

--- index.php
+++ index.php
@@ -15,8 +15,6 @@
 	$mon = strftime('%B');
 	$year = strftime('%Y');
 	
-	
-
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
