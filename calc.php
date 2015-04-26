acf5d0b4c98fc1ff644744b7ccb3a4a3befce595
danvop <danvop@gmail.com>
2015-04-26 19:42:41 +1000

    calc modified

--- calc.php
+++ calc.php
@@ -9,7 +9,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 	switch($op){
 		case '+': $output .= $n1 + $n2; break;
 		case '-': $output .= $n1 - $n2; break;
-		case '*': case '+': $output .= $n1 + $n2; break;
+		case '*': case '+': $output .= $n1 * $n2; break;
 		case '/': 
 			if ($n2 === 0)
 				$output = 'Деление на 0 запрещено';
@@ -24,11 +24,11 @@ if ($output)
 ?>
 			<form action='' method='post'>
 				<label>Число 1:</label><br />
-				<input name='num1' type='text'/><br />
-				<label>Оператор: </label><br />
-				<input name='operator' type='text'/><br />
+				<input name='num1' value="<?=$n1?>" type='text'/><br />
+				<label>Оператор:  </label><br />
+				<input name='operator' value="<?=$op?>" type='text'/><br />
 				<label>Число 2: </label><br />
-				<input name='num2' type='text'/><br /><br />
+				<input name='num2' value="<?=$n2?>" type='text'/><br /><br />
 				<input type='submit' value='Считать'>
 			</form>	
 			
